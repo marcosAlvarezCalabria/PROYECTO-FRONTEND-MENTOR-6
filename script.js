2dd4291f342d5dfc62c22c2147f8fc2f20c24174
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2023-11-24 12:27:32 +0000

    ancla ok

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-
 let accion = document.querySelector(".boton");
-let cartel= document.querySelector(".cartel")
-let michelle=document.querySelector(".name-date-button")
+let cartel = document.querySelector(".cartel");
+let michelle = document.querySelector(".name-date-button");
+let enlace1 = document.querySelector("#enlace1");
+let enlace2 = document.querySelector("#enlace2");
+let enlace3 = document.querySelector("#enlace3");
 
-accion.addEventListener("mouseover", probar); 
+accion.addEventListener("mouseover", probar);
 
-function probar(){
-    
-    var anchoVentana = window.innerWidth 
-    
-    if (anchoVentana < 1440) {
-        cartel.style.display = "block";
-        michelle.style.display = "none";
-    }
-    else{
-        cartel.style.display = "block";
-    }
+function probar() {
+  var anchoVentana = window.innerWidth;
 
+  if (anchoVentana < 1440) {
+    cartel.style.display = "block";
+    michelle.style.display = "none";
+  } else {
+    cartel.style.display = "block";
+  }
 }
 
+enlace1.addEventListener("click", (event) => {
+  console.log("hola");
+
+  cartel.style.display = "none";
+});
+enlace2.addEventListener("click", (event) => {
+  console.log("hola");
+
+  cartel.style.display = "none";
+});
+enlace3.addEventListener("click", (event) => {
+  console.log("hola");
+
+  cartel.style.display = "none";
+});
